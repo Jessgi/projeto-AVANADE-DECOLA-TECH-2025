d4ff102ea90d528ae27f55244aa9f66f6074c57f
Jessica Gizela Dev <devjessgizela@gmail.com>
2025-03-03 23:46:32 -0300

    Initial commit

---
+++ src/main/java/dio/projeto1/projeto/santander/ProjetoSantanderApplication.java
@@ -0,0 +1,13 @@
+package dio.projeto1.projeto.santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/projeto1/projeto/santander/ProjetoSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.projeto1.projeto.santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
